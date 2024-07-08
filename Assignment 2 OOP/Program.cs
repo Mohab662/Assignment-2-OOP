140ec7bce61c28c956deeb1fc4410fd7167ac9ca
Mohab662 <mohabmbelkan@gmail.com>
2024-07-08 14:23:40 +0300

    Add project files.

---
+++ Assignment 2 OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_2_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
